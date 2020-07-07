45c552f556f9a38646c7163f7a9d7ca75c8638cf
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-07 17:54:08 +0530

    added bad-word filter and restricted input to 30 sec

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const monk = require("monk");
+const Filter = require("bad-words");
+const rateLimit = require("express-rate-limit");
 
 const db = monk("localhost/meower");
 const mews = db.get("mews");
+const filter = new Filter();
 
 function isValidMew(body) {
     return (
@@ -24,12 +27,19 @@ router.get("/mews", (req, res) => {
     });
 });
 
-router.post("/mews", (req, res) => {
+const createAccountLimiter = rateLimit({
+    windowMs: 30 * 1000, // 30 sec window
+    max: 1, // start blocking after 5 requests
+    message:
+        "Too many accounts created from this IP, please try again after 30 seconds",
+});
+
+router.post("/mews", createAccountLimiter, (req, res) => {
     console.log(req.body);
     if (isValidMew(req.body)) {
         const mew = {
-            name: req.body.nameData.toString(),
-            content: req.body.contentData.toString(),
+            name: filter.clean(req.body.nameData.toString()),
+            content: filter.clean(req.body.contentData.toString()),
             created: new Date(),
         };
 
